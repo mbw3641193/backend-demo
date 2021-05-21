9b2bbe7d4f5dfe4aa44bfe1b807b7d47e4ecf242
mabowen <ethan.ma@innocellence.com>
2021-05-21 18:15:47 +0800

    finish backend api

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ import 'rimraf';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('ejs');
+  // app.useStaticAssets(join(__dirname, '..', 'public'));
+  // app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  // app.setViewEngine('ejs');
   await app.listen(process.env.PORT);
 }
 bootstrap();
